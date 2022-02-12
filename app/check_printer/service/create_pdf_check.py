236b9ae4a323559612b4b2d613d64dc80d16723b
vildan-valeev <vildan.maydanovich.valeev@gmail.com>
2022-02-13 00:59:08 +0500

    выпилил сервис - генератор пдф

--- app/check_printer/service/create_pdf_check.py
+++ app/check_printer/service/create_pdf_check.py
@@ -1,36 +1,30 @@
-import json
-import time
-
-import requests
+import pdfkit
 from django.template.loader import render_to_string
 from django_rq import job
 
 from check_printer.models import Check, CHECK_STATUS
-from src.settings import env
 
 
-# @job
+@job
 def generate_pdf(obj: Check):
     print(f'Запущена фоновая задача! создаем пдф файлы для  {obj.printer_id.name}')
     # time.sleep(5)
-    PDF_GEN_HOST = env.str('PDF_GEN_HOST')
-    PDF_GEN_PORT = env.str('PDF_GEN_PORT')
-    url = f'http://{PDF_GEN_HOST}:{PDF_GEN_PORT}/'
+
     # пропускаем данные через шаблон
     content = render_to_string(f'{obj.type}_check.html', obj.order)
-    print(content)
+    # print(content)
     # data = {'contents': open('/file/to/convert.html').read().encode('base64'),}
 
     # создаем данные для отправки на конвертацию
-    data = {'contents': content, }
-    headers = {'Content-Type': 'application/json', }
-    response = requests.post(url, data=json.dumps(data), headers=headers)
+
+
     # Save the response contents to a file
     order_id = obj.order['id']
     check_type = obj.type
-    path = f'/media/{order_id}_{check_type}.pdf'
-    with open(path, 'wb') as f:
-        f.write(response.content)
+    path = f'{order_id}_{check_type}.pdf'
+    pdfkit.from_string(content, path)
+    # with open(path, 'wb') as f:
+    #     f.write(pdf)
     # Save link and status to db
     obj.status = CHECK_STATUS[1][0]
     obj.pdf_file = path
