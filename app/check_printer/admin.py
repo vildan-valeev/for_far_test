c529d7169a1bf4f37cddbdf4b6ef366cc72d91fe
vildan-valeev <vildan.maydanovich.valeev@gmail.com>
2022-02-12 17:58:20 +0500

    фильтры в админке

--- app/check_printer/admin.py
+++ app/check_printer/admin.py
@@ -6,10 +6,12 @@ from check_printer.models import Check, Printer
 
 class CheckAdmin(admin.ModelAdmin):
     list_display = ['printer_id', 'type', 'order', 'status', 'pdf_file']
+    list_filter = ['printer_id', 'type', 'status', ]
 
 
 class PrinterAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'check_type', 'point_id']
+    list_filter = ['check_type', 'point_id']
 
 
 admin.site.register(Check, CheckAdmin)
