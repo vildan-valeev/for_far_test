d89590d2297571fa59f0c02b10f6e15b24ef25ab
vildan-valeev <vildan.maydanovich.valeev@gmail.com>
2022-02-13 02:18:57 +0500

    edit admin - add tabularInline to printers

--- app/check_printer/admin.py
+++ app/check_printer/admin.py
@@ -9,9 +9,19 @@ class CheckAdmin(admin.ModelAdmin):
     list_filter = ['printer_id', 'type', 'status', ]
 
 
+class TabChecks(admin.TabularInline):
+    model = Check
+    readonly_fields = ['printer_id', 'type', 'order', 'status', 'pdf_file']
+    can_delete = False
+    max_num = 0
+
+
 class PrinterAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'check_type', 'point_id']
     list_filter = ['check_type', 'point_id']
+    search_fields = ['api_key', 'name', ]
+    search_help_text = 'search by name and apikey'
+    inlines = [TabChecks, ]
 
 
 admin.site.register(Check, CheckAdmin)
