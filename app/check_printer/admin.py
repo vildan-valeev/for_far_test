2159e91145ed630c32bf81d5d1d33dc97b5d28d7
vildan-valeev <vildan.maydanovich.valeev@gmail.com>
2022-02-12 16:40:07 +0500

    edit admin, show models

--- app/check_printer/admin.py
+++ app/check_printer/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from check_printer.models import Check, Printer
+
+
+class CheckAdmin(admin.ModelAdmin):
+    list_display = ['printer_id', 'type', 'order', 'status', 'pdf_file']
+
+
+class PrinterAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'check_type', 'point_id']
+
+
+admin.site.register(Check, CheckAdmin)
+admin.site.register(Printer, PrinterAdmin)
